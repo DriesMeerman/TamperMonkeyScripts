62a720794887f0be2a4646bac7457ebca29c74eb
Dries Meerman <dries.meerman@hva.nl>
2015-09-28 22:01:10 +0200

    first commit added readme and a wikipedia mobile rewrite script.

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains my personal tampermonkey scripts
+None of them are going to be too complicated
+Mostly simple scripts i created to make browsing a tiny bit better
---
+++ wikipedia_mobile_rewrite.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         wikipedia mobile rewrite
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  this script is hacked together and might break on changes by wikipedia
+//               however i was sick of not getting auto redirected to desktop site for wikipedia
+// @author       You
+// @match        https://en.m.wikipedia.org/*
+// @grant        none
+// ==/UserScript==
+
+var url = window.location.href;
+var url_fixed = url.search("m.wikipedia.org");
+var new_url;
+
+if (url_fixed != -1){
+    new_url = "https://www."+url.substring(url_fixed+2, url.length);
+    rewriteUrl(new_url);
+}else{
+    //not m.wikipedia.org shouldn't ever happen
+    alert('I need an adult'); 
+}
+function rewriteUrl(url_text){
+    window.location.href = url_text;
+}
