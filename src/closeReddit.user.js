51459ea0ebd3ad7c43b884787a45800379df6228
Dries Meerman <dries.meerman@student.uva.nl>
2020-11-30 13:53:57 +0100

    Create closeReddit.user.js
    script to close reddit

---
+++ src/closeReddit.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     Close Reddit
+// @version  1
+// @grant    none
+// @include *.reddit.com/*
+// ==/UserScript==
+window.close();
+document.body.innerHTML = "No reddit for you";
